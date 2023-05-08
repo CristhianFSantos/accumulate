5ccb6c3830e64c56fbd949765f63a18401ec7e03
Cristhian Santos <cristhianfelipe0@gmail.com>
2023-05-08 11:50:32 -0300

    ✨ feat: initial base setup api

--- accumulate-web/src/app/domain/accumulate/pages/dashboard/dashboard.component.ts
+++ accumulate-web/src/app/domain/accumulate/pages/dashboard/dashboard.component.ts
@@ -1,10 +1,20 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { Component, OnInit, inject } from '@angular/core';
 
 @Component({
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.scss'],
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, HttpClientModule],
 })
-export class DashboardComponent {}
+export class DashboardComponent implements OnInit {
+  private readonly httpClient = inject(HttpClient);
+  application: string;
+
+  ngOnInit(): void {
+    this.httpClient.get('http://localhost:3000').subscribe((data: any) => {
+      this.application = data['application'] as string;
+    });
+  }
+}
